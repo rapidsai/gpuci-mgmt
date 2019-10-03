6de063ab91615763f94de516f0c53c6366627091
Mike Wendt <mike.wendt@me.com>
2019-10-03 12:35:15 -0400

    FIX Remove old code

--- init/aws-nvme.sh
+++ init/aws-nvme.sh
@@ -28,22 +28,6 @@ logger "Add delay for cron apt-get update/upgrade job"
 sudo sed -i '2s/.*/sleep 900/' /etc/cron.daily/apt-compat
 sudo service cron restart
 
-logger "Wait for system apt-get update/upgrade to finish"
-i=0
-tput sc
-while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
-    case $(($i % 4)) in
-        0 ) j="-" ;;
-        1 ) j="\\" ;;
-        2 ) j="|" ;;
-        3 ) j="/" ;;
-    esac
-    tput rc
-    echo -en "\r[$j] Waiting for other software managers to finish..." 
-    sleep 1
-    ((i=i+1))
-done
-
 logger "Update/upgrade image first; before unattended-upgrades runs"
 apt-butler update
 apt-butler upgrade -y
