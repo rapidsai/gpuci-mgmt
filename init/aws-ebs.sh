06e8122aaac2114b6096e5b0714891b9ada7e5f2
Mike Wendt <mike.wendt@me.com>
2019-10-02 23:29:22 -0400

    ENH Add awscli for S3 uploads and logger debug

--- init/aws-ebs.sh
+++ init/aws-ebs.sh
@@ -5,19 +5,29 @@
 # AWS init script for gpuCI nodes with EBS only storage
 #
 
+SCRIPT_NAME="$0"
+function logger {
+  TS=`date +%F_%H-%M-%S`
+  echo "[$SCRIPT_NAME $TS] $@"
+}
 
-# Update/upgrade image first; before unattended-upgrades runs
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get clean
+logger "Update/upgrade image first; before unattended-upgrades runs"
+sudo apt-get update && sudo apt-get upgrade -y
 
-# Check mounts
+set -e
+
+logger "Check mounts"
 mount
 df -h
 
-# Ensure ubuntu user has full rights on directory for Jenkins work
+logger "Install awscli"
+sudo apt-get install awscli -y
+
+logger "Ensure ubuntu user has full rights on directory for Jenkins work"
 sudo mkdir -p /jenkins
 sudo chown -R ubuntu:ubuntu /jenkins
 
-# Override docker setup and utilize internal docker registry mirror
+logger "Override docker setup and utilize internal docker registry mirror"
 sudo service docker stop
 sudo cat /etc/docker/daemon.json
 cat <<EOL > /tmp/daemon.json
@@ -30,9 +40,9 @@ sudo mv /tmp/daemon.json /etc/docker/daemon.json
 sudo cat /etc/docker/daemon.json
 sudo service docker start
 
-# Ensure docker system is clean
+logger "Ensure docker system is clean"
 docker system prune -f
 
-# Connect node to Jenkins
+logger "Connect node to Jenkins"
 wget https://gpuci.gpuopenanalytics.com/plugin/ec2/AMI-Scripts/ubuntu-ami-setup.sh
 sudo sh ubuntu-ami-setup.sh gpuci.gpuopenanalytics.com
