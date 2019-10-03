321b42e6f5b32553bb0c16f8be7dfba265bb0d79
Mike Wendt <mike.wendt@me.com>
2019-10-03 16:40:28 -0400

    FIX Correct apt lock check

--- init/aws-ebs.sh
+++ init/aws-ebs.sh
@@ -15,8 +15,8 @@ function logger {
 function apt-butler {
   logger "apt-butler tasked to run 'sudo apt-get ${@}'"
   i=0
-  while `sudo lsof /var/lib/dpkg/lock | grep "apt"` ; do
-      logger "apt-butler waiting for other software managers to finish..."
+  while sudo lsof /var/lib/dpkg/lock* 2>&1 > /dev/null ; do
+      logger "apt-butler ... waiting apt instances to finish ..."
       sleep 5
       ((i=i+1))
   done
