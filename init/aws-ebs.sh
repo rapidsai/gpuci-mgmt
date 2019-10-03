6104052226ac4124989a2ae6ca18619095c84371
Mike Wendt <mike.wendt@me.com>
2019-10-03 12:31:56 -0400

    FIX Update butler script

--- init/aws-ebs.sh
+++ init/aws-ebs.sh
@@ -4,6 +4,7 @@
 #
 # AWS init script for gpuCI nodes with EBS only storage
 #
+set -ex
 
 SCRIPT_NAME="$0"
 function logger {
@@ -14,20 +15,14 @@ function logger {
 function apt-butler {
   logger "apt-butler tasked to run 'sudo apt-get ${@}'"
   i=0
-  tput sc
-  while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
-      case $(($i % 4)) in
-          0 ) j="-" ;;
-          1 ) j="\\" ;;
-          2 ) j="|" ;;
-          3 ) j="/" ;;
-      esac
-      tput rc
-      echo -en "\r[$j] Waiting for other software managers to finish..." 
-      sleep 1
+  while `sudo lsof /var/lib/dpkg/lock | grep "apt"` ; do
+      logger "apt-butler waiting for other software managers to finish..."
+      sleep 5
       ((i=i+1))
   done
+  logger "apt-butler running 'sudo apt-get ${@}'"
   sudo apt-get ${@}
+  logger "apt-butler finished 'sudo apt-get ${@}'"
 }
 
 logger "Add delay for cron apt-get update/upgrade job"
@@ -37,8 +32,6 @@ sudo service cron restart
 logger "Update/upgrade image first; before unattended-upgrades runs"
 apt-butler update
 apt-butler upgrade -y
-
-set -e
 
 logger "Check mounts"
 mount
